cd733811e4deb92b8ef41c1479ac25f4d4a2350e
joserra <joserra71@gmail.com>
2024-04-30 12:33:51 -0600

    Arbolito con Python

--- sesion04.py
+++ sesion04.py
@@ -1,12 +1,6 @@
-# Example of execution flow
-# Instruction 1
-print("Start of the program")
- 
-# Instruction 2
-print(" Print second instruction")
- 
-# Instruction 3
-print("Hello, World!")
- 
-# Instruction 4
-print("End of the program")
+#Example Christmas Tree
+print("    *    ")
+print("   ***   ")
+print("  *****  ")
+print(" ******* ")
+print("   | |   ")
